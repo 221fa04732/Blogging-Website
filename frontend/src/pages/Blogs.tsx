d2867f84a0070814ab74030659cfcc40fe9e339e
221fa04732 <221fa04732@gmail.com>
2025-01-23 14:15:03 +0530

    loader page and latest blog first show updated

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -55,33 +55,34 @@ export default function Blogs() {
 
   }, [loading]); 
 
-  return (
-    <div className="bg-stone-800 min-h-screen flex flex-col items-center">
-      <div className="w-full min-h-16 max-h-16 fixed bg-stone-800 border-b border-blue-500 flex justify-center">
-        <Header />
-      </div>
-      <div className="mt-16 flex flex-col items-center">
-        {blogs.length > 0 ? (
-          blogs.map((blog) => (
-            <BlogCard
-              key={blog.id}
-              id={blog.id}
-              postDate={blog.publishDate.toString()}
-              title={blog.title}
-              content={blog.content}
-              name={blog.author.name}
-            />
-          ))
-        ) : (
-          < Loader />
-        )}
-      </div>
-      <div className={`bg-white ${postVisible === 1 ? "block" : "hidden"} fixed h-screen w-full md:w-10/12 rounded-lg z-30`}>
-        <Post />
-      </div>
-      <div className="w-full flex justify-center">
-        <Footer />
-      </div>
+  return (<div className="bg-stone-800 min-h-screen flex flex-col items-center">
+        <div className={`bg-stone-800 min-h-screen flex flex-col items-center ${postVisible === 1 ? "blur-sm" : "blur-none"} w-full`}>
+          <div className="w-full min-h-16 max-h-16 fixed bg-stone-800 border-b border-blue-500 flex justify-center">
+            <Header />
+          </div>
+          <div className="mt-16 flex flex-col items-center w-full">
+            {!loading ? (
+              [...blogs].reverse().map((blog) => (
+                <BlogCard
+                  key={blog.id}
+                  id={blog.id}
+                  postDate={blog.publishDate.toString()}
+                  title={blog.title}
+                  content={blog.content}
+                  name={blog.author.name}
+                />
+              ))
+            ) : (
+              < Loader />
+            )}
+          </div>
+          <div className="w-full flex justify-center">
+            <Footer />
+          </div>
+        </div>
+        <div className={`bg-white ${postVisible === 1 ? "block" : "hidden"} fixed h-screen w-full md:w-10/12 rounded-lg z-30`}>
+            <Post />
+        </div>
     </div>
   );
 }
