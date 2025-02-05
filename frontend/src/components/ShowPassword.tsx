d9833441f85cd58c64f7811961f71c316b04e63d
221fa04732 <221fa04732@gmail.com>
2025-02-05 19:39:36 +0530

    visible eye img logic flipped

--- frontend/src/components/ShowPassword.tsx
+++ frontend/src/components/ShowPassword.tsx
@@ -9,6 +9,6 @@ export default function ShowPassword(){
         onClick={()=>{
             visible === 0 ? setVisible(1) : setVisible(0)
         }}>
-        {<img className='min-h-8 max-h-8 min-w-8 max-w-8 p-1' src={visible === 0 ? "/eye-solid.svg" : "/eye-slash-solid.svg"}/>}
+        {<img className='min-h-8 max-h-8 min-w-8 max-w-8 p-1' src={visible === 1 ? "/eye-solid.svg" : "/eye-slash-solid.svg"}/>}
     </button>)
 }
